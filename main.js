0bd2d29d539ba7cfb863a77ef426456cc6dcdf5a
Lian Martins <allamg2003@icloud.com>
2023-11-16 20:55:09 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+const sqlite3 = require("sqlite3");
+const express = require("express")
+const app = express()
+
+const client = new sqlite3.Database("./data/database.sqlite")
+
+client.run('create table if not exists client (id integer primary key, name text)')
+
+app.get("/add", ()=>{
+    client.run('insert into client (name) values (?)', ['Alice'])
+})
+
+const port = process.env.PORT || 5000
+app.listen(port, ()=>{
+    console.log(`server on, port:${port}`)
+})
